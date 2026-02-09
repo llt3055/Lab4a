78ec499ba69bf910bd594cbcf9d731e7fb102bc3
llt3055 <llt30555@gmail.com>
2026-02-09 13:36:18 +0800

    Milestone 2: tested vector by poulating it with a color object

--- lab4a.cpp
+++ lab4a.cpp
@@ -16,9 +16,17 @@ struct Color{
 
 int main(){
     srand(time(0));
+    //milestone 1 test initialization color
     Color color = {255, 0, 0};
     cout << "Milestone 1 - Initialized Color: "
     << "R: " << color.red
     << ", G: " << color.green 
     << ", B: " << color.blue << endl;
+
+    vector<Color> colors;
+    cout << colors.size() << endl;
+    cout << "Stored color: "<<
+    "R=" << colors[0].red 
+    << " G=" << colors[0].green 
+    << " B=" << colors[0].blue << endl;
 }
