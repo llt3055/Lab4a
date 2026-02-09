a7c284221da7df97400451a29b96c636a7157f48
llt3055 <llt30555@gmail.com>
2026-02-09 13:06:13 +0800

    included libraries and defined Color struct

--- lab4a.cpp
+++ lab4a.cpp
@@ -1,1 +1,19 @@
 // COMSC-210 | Lab 4a | Tianyi Cao
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+struct Color{
+    int red;
+    int green;
+    int blue;
+};
+
+int main(){
+    
+}
